8b93b5018d3a1c8be78e8a491b3494018525da0f
Edwin Renck <edrenck@gmail.com>
2021-10-21 00:35:26 -0700

    removed possible bad import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const api = require("./api");
 const cors = require("cors");
-const { response } = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
